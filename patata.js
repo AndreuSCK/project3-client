5e000d60def371c861e5648521af4e5423ce2249
Andreu <andreu.sck@gmail.com>
2020-11-20 04:47:53 +0100

    main

---
+++ patata.js
@@ -0,0 +1,19 @@
+let grid = [
+    ['', ''],
+    ['', '']
+]
+let gridSize = [3, 2]
+
+let patatas = { texto: 'patatas y puerros' }
+
+let fotos = { foto: 'viajedeverano.png' }
+
+
+console.log(JSON.stringify(patatas))
+console.log(JSON.stringify(fotos))
+
+// console.log(patatas)
+
+// console.log(patatas.keys)
+
+console.log(Object.values(fotos))
